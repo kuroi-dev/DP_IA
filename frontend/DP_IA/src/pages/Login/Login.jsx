d1e02c9254d92bf3fa493512bd5d8fcac7242550
David <david.riquelme.sb@gmail.com>
2025-12-10 14:10:07 -0300

    UPGRADE: responsive colapse

--- frontend/DP_IA/src/pages/Login/Login.jsx
+++ frontend/DP_IA/src/pages/Login/Login.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import "./Login.css";
+import { Container, Row, Col, Form, Button } from "react-bootstrap"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -12,35 +14,31 @@ function Login() {
   };
 
   return (
-    <div className="login-container">
-      <div className="login-inter-container position1">
-        <div className="login-inter2-container">
-          <div className="login-inter3-container tamano1">
-            <div className="login-inter4-container tamano1-1">
-              <div></div>
-              <div><h1>Municipalidad de Villarrica</h1></div>
-            </div>
-            <div className="login-inter4-container tamano1-2">
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci quae mollitia eaque aliquam fugiat, a tenetur soluta nemo molestias ratione obcaecati repellendus, laudantium ex quidem voluptatibus, eos culpa aspernatur beatae.</p>
-            </div>
-            <div className="login-inter4-container tamano1-3">
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-            </div>
+    <Container fluid className="login-container">
+      <Row className="vh-100 align-items-center g-4">
+        <Col md={6}  className="text-end p-4 limitCol">
+          <div>
+            <img src="" alt="Logo" className="img-fluid mb-3" />
+            <h1>Municipalidad de Villarrica</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam dignissimos corporis mollitia, cum blanditiis nesciunt, odio explicabo iure sed quisquam veniam dolor debitis vero aliquam ut eum voluptatibus, itaque facilis.</p>
           </div>
-        </div>
-      </div>
-      <div className="login-inter-container position2">
-        <div className="login-inter2-container">
-          <div className="login-inter3-container tamano2">
-            
-          </div>
-        </div>
-      </div>
-      
-    </div>
+        </Col>
+
+
+        <Col md={6}  className="d-flex justify-content-center limitCol">
+          <Form className="login-form">
+            <h2>Iniciar Sesión</h2>
+            <Form.Group className="mb-3">
+              <Form.Control type="text" placeholder="Usuario" />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Control type="password" placeholder="Contraseña" />
+            </Form.Group>
+            <Button type="submit">Ingresar</Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
