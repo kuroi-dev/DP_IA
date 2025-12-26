5e30a65b7a19f51729e1e1e464f9eab5db345718
David <david.riquelme.sb@gmail.com>
2025-12-26 18:25:47 -0300

    Upgrade Login.jsx

--- frontend/DP_IA/src/pages/Login/Login.jsx
+++ frontend/DP_IA/src/pages/Login/Login.jsx
@@ -3,6 +3,10 @@ import "./Login.css";
 import { Container, Row, Col, Form, Button } from "react-bootstrap"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logoEmpresa from '../../assets/images/logo.png';
+import logoFacebook from '../../assets/images/face.webp';
+import logoInstagram from '../../assets/images/insta.webp';
+import logoYoutube from '../../assets/images/you.png';
+import logoX from '../../assets/images/x.webp';
 
 
 function Login() {
@@ -16,35 +20,64 @@ function Login() {
   };
 
   return (
-    <Container fluid className="login-container">
-      <Row className="vh-100 align-items-center g-0 limitCol0">
-        <Col  className="text-end p-4 limitCol colortest">
-          <div>
-            <div className="flexDisplay">
-              <img src={logoEmpresa} alt="Logo" className=" logoLogin" />
-              <h1>Municipalidad de Villarrica</h1>
+    <div className="content-fondo">
+      <div className="login-container">
+          <div className="inter-content-login tamano1">
+            <div className="content-logoAndText">
+              <div className="flexDisplay">
+                <div className="content-logo">
+                  <img src={logoEmpresa} alt="Logo" className=" logoLogin" />
+                </div>
+                
+                <h1>Municipalidad de Villarrica</h1>
+              </div>
+              
+              <div className="content-parrafo">
+                <p>Plataforma interna de la Municipalidad de Villarrica que permite la lectura y gestión de documentos oficiales, facilitando además la generación automática de podcasts en audio.</p>
+              </div>
+              <div className="content-socialmedia">
+                <div className="socialmedia"><img className="logoSocial" src={logoFacebook} alt="Facebook" /></div>
+                <div className="socialmedia"><img className="logoSocial" src={logoInstagram} alt="Instagram" /></div>
+                <div className="socialmedia"><img className="logoSocial" src={logoYoutube} alt="YouTube" /></div>
+                <div className="socialmedia"><img className="logoSocial" src={logoX} alt="X" /></div>
+              </div>
             </div>
-            
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam dignissimos corporis mollitia, cum blanditiis nesciunt, odio explicabo iure sed quisquam veniam dolor debitis vero aliquam ut eum voluptatibus, itaque facilis.</p>
           </div>
-        </Col>
+          
+          <div className="inter-content-login tamano2">   
+            <Form className="login-form">
+              <div className="content-user-login tamano3"> 
+                <h2>Iniciar Sesión</h2>
+              </div>
+              <div className="content-user-login tamano4"> 
+                <Form.Group>
+                  <p>Usuario</p>
+                  <Form.Control type="text" placeholder="" />
+                </Form.Group>
+              </div>
+              <div className="content-user-login tamano4"> 
+                <Form.Group>
+                  <p>Contraseña</p> 
+                  <Form.Control type="password" placeholder="" />
+                </Form.Group>
+              </div>
+              <div className="content-user-login tamano5"> 
+                <input type="checkbox" /><p>Recordar esta cuenta</p>
+              </div>
+              <div className="content-user-login tamano6"> 
+                <Button type="submit">Iniciar Sesión</Button>
+              </div>
+              <div className="content-user-login tamano7"> 
+                <p>Al hacer clic en "Iniciar sesión", aceptas los <br /> <b>Términos de servicio</b> | <b>Política de privacidad</b></p>
+              </div>
+                
+              
 
+            </Form>
 
-        <Col  className="d-flex justify-content-center colortest limitCol " >
-          <Form className="login-form">
-            <h2>Iniciar Sesión</h2>
-            <Form.Group className="mb-3">
-              <Form.Control type="text" placeholder="Usuario" />
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Control type="password" placeholder="Contraseña" />
-              <h1></h1>
-            </Form.Group>
-            <Button type="submit">Ingresar</Button>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
+          </div>
+      </div>
+    </div>
   );
 }
 
