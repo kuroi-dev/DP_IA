0fcc6e35511f7e745850f73929d8bae92713b336
David <david.riquelme.sb@gmail.com>
2025-12-30 19:31:18 -0300

    UPGRADE : Dashboard.jsx + css

--- frontend/DP_IA/src/pages/Dashboard/Dashboard.jsx
+++ frontend/DP_IA/src/pages/Dashboard/Dashboard.jsx
@@ -1,43 +1,56 @@
 import React, { useEffect, useState } from "react";   
 import "./Dashboard.css"; 
+import logo from '../../assets/images/logo.png';
+import logoUser from '../../assets/images/icoUser.webp';
 
 function Dashboard() {
   const [isVisible, setIsVisible] = useState(true);
+  const [nameUser, setNameUser] = useState("");
 
   const timeOutLogin = () => {
     setTimeout(() => {
       setIsVisible(false);
+      
       console.log("Timeout completed, state updated.");
     }, 5000); 
   }
 
   useEffect(() => {
     timeOutLogin();
+    setNameUser("Megan Ramirez");
   }, []);
 
   return (
     <div className="dashboard-container">
+      <header className="headerDashboard">
+        <div className="interHeader1">
+          <img src={logo} className="logoMuni" />
+          <h2>Municipalidad de Villarrica</h2>  
+        </div>
+        <div className="interHeader2">
+          <button className="btnUser"><img src={logoUser} className="logoUser" /></button>
+        </div>
+      </header>
       <div
-        style={{
-          opacity: isVisible ? 1 : 0,
-          transition: 'opacity 3s ease-out',
-          visibility: isVisible ? 'visible' : 'hidden'
-        }}
+        className={`fade-transition ${isVisible ? 'fade-visible' : 'fade-hidden'}`}
       >
-        <h1>Bienvenido Dashboard</h1>
+        <h1 className="nombreUser">Bienvenido {nameUser}</h1>   
         <p>Aquí podrás acceder a tus datos y funcionalidades principales.</p>
       </div>
 
       <div
-        style={{
-          opacity: isVisible ? 0 : 1,
-          transition: 'opacity 3s ease-out',
-          visibility: isVisible ? 'hidden' : 'visible'
-        }}
+        className={`fade-transition ${isVisible ? 'fade-hidden' : 'fade-visible'}`}
       >
-        <h1>Dashboard</h1>
-        <p>testing</p>
-        <p>testing</p>
+        <div className="contentPDFUp">
+          <div className="contentPDFIcon">
+
+          </div>
+          <div className="contentPDFText">
+            <h1>Dashboard Principal</h1>
+            <p>Explora las diferentes secciones y herramientas disponibles.</p>
+          </div>
+          
+        </div>
       </div>
     </div>
   );
